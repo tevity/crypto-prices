1307fe9aefa328e723f356b6c6e87ee354f9d219
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-21 15:57:47 +0100

    Added material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule, httpInterceptorProviders } from '@core';
 import { PricesModule } from 'src/prices';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { PricesModule } from 'src/prices';
     HttpClientModule,
     AppRoutingModule,
     CoreModule,
-    PricesModule
+    PricesModule,
+    BrowserAnimationsModule
   ],
   providers: [
     httpInterceptorProviders
