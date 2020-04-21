14166c24333963c6c8519116c9b351761c92a3fb
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-21 11:35:42 +0100

    Updated karma launcher to work in docker

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -25,8 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessRootCheck'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessRootCheck: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
