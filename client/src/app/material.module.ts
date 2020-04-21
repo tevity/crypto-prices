fb6746007123f5c566505034fd43d6728a69cc63
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-21 16:42:55 +0100

    Some more styling

--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
     imports: [
+        MatCardModule,
         MatTableModule
     ],
     exports: [
+        MatCardModule,
         MatTableModule
     ]
 })
